6b201c9494dd9db05c8dbbf1b6fd1c52f70ac86e
Yarikks <jasper18poil@gmail.com>
2021-02-17 23:36:07 +0200

    Make use of dynamic data

--- src/containers/Header/index.jsx
+++ src/containers/Header/index.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import styles from './header.module.scss';
 
-const Header = props => {
+const Header = ({ messages }) => {
+  const messagesCount = messages.length;
+
+  const usersCount = () => {
+    const uniqueUsers = [...new Set(messages.map(message => message.userId))];
+    return uniqueUsers.length;
+  }
+
+  const lastMessage = () => {
+    const lastMesage = messages[messages.length - 1];
+    if(lastMesage) {
+      return new Date(lastMesage.createdAt).toLocaleTimeString();
+    }
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.info}>My chat</div>
-      <div className={styles.info}>23 participants</div>
-      <div className={styles.info}>53 messages</div>
-      <div className={styles.lastMessage}>last message at 14:28</div>
+      <div className={styles.info}>{usersCount()} participants</div>
+      <div className={styles.info}>{messagesCount} messages</div>
+      <div className={styles.lastMessage}>last message at {lastMessage()}</div>
     </div>
   )
 };
