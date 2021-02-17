cbf32f7b5dc7309731a952d1db08d76ba8a257b5
Yarikks <jasper18poil@gmail.com>
2021-02-17 23:33:57 +0200

    Update MessageList logic and styles

--- src/containers/MessageList/index.jsx
+++ src/containers/MessageList/index.jsx
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import PropTypes from 'prop-types';
+import Message from "../../components/Message";
 import styles from './messageList.module.scss';
+import DateDivider from "../../components/DateDivider";
 
-const MessageList = props => {
+const MessageList = ({ messages, onDelete, onEdit }) => {
+  const lastMessageRef = useRef(null);
+
+  const showMessage = (message, index) => {
+    const prevDate = index > 0 
+    ? new Date(messages[index-1].createdAt).toLocaleDateString()
+    : new Date(messages[0].createdAt).toLocaleDateString();
+
+    const currDate = new Date(message.createdAt).toLocaleDateString();
+    if(prevDate === currDate)
+    {
+      return (
+        <div key={index} style={{display: 'grid'}} ref={lastMessageRef}>
+          <Message message={message} deleteMessage={onDelete} editMessage={onEdit} />
+      </div>
+      )
+    } else {
+      return (
+        <div key={index} style={{display: 'grid'}} ref={lastMessageRef}>
+          <DateDivider date={currDate} />
+          <Message message={message} deleteMessage={onDelete} editMessage={onEdit} />
+        </div>
+      );
+    }
+  }
+
+  useEffect(() => {
+    if(lastMessageRef) {
+      lastMessageRef.current?.scrollIntoView();
+    }
+  }, [messages])
+
   return (
-    <div className={styles.listContainer}></div>
+    <div className={styles.listContainer}>
+      { messages
+        ? messages.map(showMessage)
+        : <div>Loading...</div>
+      }
+    </div>
   )
+};
+
+MessageList.propTypes = {
+  messages: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default MessageList;
