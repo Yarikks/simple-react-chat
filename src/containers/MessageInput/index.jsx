aa436d4ac44d5a0a3a8b326390bea3026029b90e
Yarikks <jasper18poil@gmail.com>
2021-02-17 23:35:08 +0200

    Update MessageInput logic and styles

--- src/containers/MessageInput/index.jsx
+++ src/containers/MessageInput/index.jsx
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as uuid } from 'uuid';
+import SendButton from "../../components/SendButton";
 import styles from './messageInput.module.scss';
 
-const MessageInput = props => {
+const MessageInput = ({ onAddMessage }) => {
+  const [text, setText] = useState('');
+
+  const addMessage = () => {
+    let message = text.trim();
+    if(message) {
+      const data = {
+        id: uuid(),
+        userId: uuid(),
+        avatar: "",
+        user: "",
+        text: message,
+        createdAt: new Date(Date.now()).toLocaleString(),
+        editedAt: "",
+        self: true
+      }
+      onAddMessage(data);
+      setText('');
+    }
+  }
   return (
     <div className={styles.inputContainer}>
-      <textarea />
-      <button className={styles.sendBtn}>
-        <i class="fas fa-arrow-alt-circle-up"></i>  
-        Send
-      </button>
+      <textarea value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter your message..." />
+      <SendButton 
+        iconClass="fa-arrow-alt-circle-up" 
+        title="Send"
+        onClick={addMessage}
+      />
     </div>
   )
 };
